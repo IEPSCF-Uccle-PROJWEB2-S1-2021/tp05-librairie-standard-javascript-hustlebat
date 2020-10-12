faa04b6ad8e8087b28f9fe4c3f249f107b80cdc0
PROJWEB batumeni <Bajo2204@student.iepscf-uccle.be>
2020-10-12 19:36:52 +0200

    notFinished

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,31 @@
 function chuckNorris(msg) {
-  return "";
+  let buffer = "";
+  for (let i = 0; i < msg.length; i++) {
+    const charCode = msg.charCodeAt(i);
+    const binaryCode = charCode.toString(2);
+    buffer += binaryCode;
+  }
+  const convertedChar = CToUnary(buffer);
+
+  return convertedChar;
+}
+
+function CToUnary(binaryCode) {
+  let result = "";
+  let lastChar = "";
+  for (let i = 0; i < binaryCode.length; i++) {
+    const element = binaryCode[i];
+    if (element === lastChar) {
+      result += "0";
+    } else {
+      result += " ";
+      if (element === "1") {
+        result += "0 0";
+      } else {
+        result += "00 0";
+      }
+    }
+    lastChar = element;
+  }
+  return result;
 }
--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,21 @@
+result
+
+
 function analyzeTemperature(quantity, data) {
-  return 0;
+  if (quantity === 0 ) 
+  console.log(quantity);
+  
+  for (let i = 0; i < quantity.length; i++) {
+    const data = array[1];
+  }
+
+  if (Math.abs(result)) > Math.abs(data) {
+    if (data > 0) { 
+      result -data
+      
+    }
+  }
+ else {   
+  
+  return 0;}
 }
